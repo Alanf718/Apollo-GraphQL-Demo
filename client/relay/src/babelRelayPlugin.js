8bf58a14ded4bff50d7b5d7f1676540495e45f95
Alanf718 <alan.fernandez@hbo.com>
2017-03-28 14:37:34 -0400

    removed relay folder

--- client/relay/src/babelRelayPlugin.js
+++
@@ -1,3 +0,0 @@
-const getBabelRelayPlugin = require('babel-relay-plugin');
-const schemaData = require('./../../../server/graphql/data/schema.json');
-module.exports = getBabelRelayPlugin(schemaData.data);
